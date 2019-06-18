ba73a15e813617ab308688bb9160873380e42b14
Olivier CANO <kindermoumoute@gmail.com>
2019-06-18 18:21:55 +0200

    address quentin's comment

--- scaleway/helpers.go
+++ scaleway/helpers.go
@@ -347,6 +347,5 @@ func regionSchema() *schema.Schema {
 
 // getRandomName returns a random name prefixed for terraform.
 func getRandomName(prefix string) string {
-
 	return namegenerator.GetRandomName("tf", prefix)
 }
